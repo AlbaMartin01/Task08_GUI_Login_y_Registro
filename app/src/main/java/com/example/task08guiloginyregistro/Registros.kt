7f36a67d8daa38687465172c7dec14ccc9b834f2
equus2001 <62957466+equus2001@users.noreply.github.com>
2023-01-27 20:01:19 +0100

    Commit Registro terminado y comentarios

--- app/src/main/java/com/example/task08guiloginyregistro/Registros.kt
+++ app/src/main/java/com/example/task08guiloginyregistro/Registros.kt
@@ -1,11 +1,34 @@
 package com.example.task08guiloginyregistro
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class Registros : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registros)
+        var recogidaArray = intent.getStringArrayListExtra("ArrayUsuarios")
+
+        //Al pulsar el boton de registrar, se guarda el nombre de usuario en el array y se pasa a la actividad vacía
+
+        findViewById<Button>(R.id.registrar).setOnClickListener{
+            recogidaArray?.add(findViewById<TextView>(R.id.nombreU).toString())
+            var intento = Intent(this, Siguiente::class.java)
+            startActivity(intento)
+        }
+
+        //Al pulsar el boton de cancelar, vuelve a la pantalla inicial (la pantalla de login)
+
+        findViewById<Button>(R.id.cancelar).setOnClickListener{
+            var intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+        }
     }
 }
